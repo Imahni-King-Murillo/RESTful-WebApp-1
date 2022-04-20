a484a4e8f46866778b80834bb9b361320c9d040c
imahni king-murillo <imahniking@gmail.com>
2022-04-20 14:17:46 -0700

    initial commit

---
+++ src/main/java/com/mountmystic/mountMysticWebsite/MountMysticWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.mountmystic.mountMysticWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountMysticWebsiteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MountMysticWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountmystic/mountMysticWebsite/MountMysticWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountmystic.mountMysticWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountMysticWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
